e12689b025b5b8cc35722951acbfa8cd3ec1c9d9
Mike <4xgood@gmail.com>
2021-12-28 05:45:02 -0700

    4:56:36 defined sql and text connections

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -8,17 +8,22 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; }
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
 
         public static void InitializeConnections (bool database, bool textFiles)
         {
             if(database)
             {
                 // TODO - create SQL connection
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+
             }
             if(textFiles)
             {
                 // TODO - crate text files
+                TextConnection text = new TextConnection();
+                Connections.Add(text);
             }
         }
     }
