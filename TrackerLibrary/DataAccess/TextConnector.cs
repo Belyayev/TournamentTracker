7838e8ec4d3b3342c0a755bccbd0b45a56dea2e1
Mike <4xgood@gmail.com>
2022-01-02 11:09:40 -0700

    7:42:00 saving Prizes to text file is working

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -1,6 +1,7 @@
 ﻿using TrackerLibrary.Models;
 using TrackerLibrary.DataAccess.TextHelpers;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
@@ -10,13 +11,26 @@ namespace TrackerLibrary.DataAccess
         //TODO - wire up CreatePrize for text files
         public PrizeModel CreatePrize(PrizeModel model)
         {
+            //Load the text file and convert the text file to List<PrizeModel>
             List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
-            //Load the text file
-            //Convert the text file to List<PrizeModel>
+
             //Find the max ID
+            int currentId = 1;
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+
+            }
+
+            model.Id = currentId;
+
             //Add the new record with the new ID (max +1)
+            prizes.Add(model);
+
             //Convert the prizes to List<string>
             //Save the List<string> to the text file
+
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
--- TrackerUi/Program.cs
+++ TrackerUi/Program.cs
@@ -16,7 +16,7 @@ namespace TrackerUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Sql);
+            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.TextFile);
             Application.Run(new CreatePrizeForm());
             //Application.Run(new TournamentDashboardForm());
         }
